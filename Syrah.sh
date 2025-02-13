4c57cba647397b16233783fb2423639f489cdf5e
Carolyn Brewster <carolynbrewster@gmail.com>
2025-02-12 19:08:22 -0600

    Update Syrah.sh

--- Syrah.sh
+++ Syrah.sh
@@ -39,11 +39,15 @@ do
   
   cd "${writeDir}"
   
+  if [ ! -d "${writeDir}intermediate_files" ]; then
+    mkdir "${writeDir}intermediate_files"
+  fi
+  
   # DETERMINE READ 1 VERSION
-  if [ ! -f "${writeDir}${batchName}_r1_version.txt" ] || [ "$resume" = false ]; then
+  if [ ! -f "${writeDir}intermediate_files/${batchName}_r1_version.txt" ] || [ "$resume" = false ]; then
     resume=false
     Rscript "${syrahDir}determine_version.R" "${manifestFile}"
-    echo "Nucleotide frequency plot at ${writeDir}${batchName}_estimated_read_1_nucleotide_frequencies.png"
+    echo "Nucleotide frequency plot at ${writeDir}intermediate_files/${batchName}_estimated_read_1_nucleotide_frequencies.png"
   else
     echo ""
     echo "Looks like read1 version already detected! Moving on..."
@@ -51,17 +55,17 @@ do
   fi
   
   # DEFORK MAPPING
-  if [ ! -f "${writeDir}${batchName}_defork_groups.txt" ] || [ "$resume" = false ]; then
+  if [ ! -f "${writeDir}intermediate_files/${batchName}_deduplication_map.txt" ] || [ "$resume" = false ]; then
     resume=false
-    Rscript "${syrahDir}create_defork_map.R" "${manifestFile}"
+    Rscript "${syrahDir}create_bead_deduplication_map.R" "${manifestFile}"
   else
     echo ""
-    echo "Looks like defork groups were already found! Moving on..."
+    echo "Looks like bead deduplication maps were already computed! Moving on..."
     echo "  (Delete files from previous runs or use 'resume=false' to change this behavior)"
   fi
 
   # BARCODE MATCHING + CORRECTION WHITELIST
-  if [ ! -f "${writeDir}${batchName}_barcode_whitelist.txt"  ] || [ "$resume" = false ]; then
+  if [ ! -f "${writeDir}intermediate_files/${batchName}_barcode_whitelist.txt"  ] || [ "$resume" = false ]; then
     resume=false
     Rscript "${syrahDir}generate_bead_barcode_whitelist.R" "${manifestFile}"
   else
@@ -72,12 +76,9 @@ do
   
 
   # READ BARCODE EXTRACTION + CORRECTION
-  if [ ! -f "${writeDir}${batchName}_r2_barcode_tagged.fastq" ] || [ "$resume" = false ]; then
+  if [ ! -f "${writeDir}intermediate_files/${batchName}_r2_barcode_tagged.fastq" ] || [ "$resume" = false ]; then
     resume=false
     Rscript "${syrahDir}extract_bead_barcodes.R" "${manifestFile}"
-    if [ "$doNonSyrah" = true ]; then
-      bash "${syrahDir}extract_bead_barcodes_nonSyrah.sh" "${manifestFile}"
-    fi
   else
     echo ""
     echo "Looks like we already added barcodes to read 2! Moving on..."
@@ -85,7 +86,7 @@ do
   fi
   
   # STAR ALIGNMENT
-  if [ ! -f "${writeDir}${batchName}_Aligned.sortedByCoord.out.bam" ] || [ "$resume" = false ]; then
+  if [ ! -f "${writeDir}intermediate_files/${batchName}_Aligned.sortedByCoord.out.bam" ] || [ "$resume" = false ]; then
     resume=false
     bash "${syrahDir}STAR_alignment.sh" "${manifestFile}"
   else
@@ -100,9 +101,9 @@ do
     bash "${syrahDir}quantify_counts.sh" "${manifestFile}"
     Rscript "${syrahDir}graphical_outputs.R" "${manifestFile}"
     echo "${batchName} ALL DONE! Find your counts matrix at ${writeDir}${batchName}_counts.tsv.gz"
-    echo "and a summary of results at ${writeDir}{batchName}_Syrah_results_summary.pdf"
+    echo "and a summary of results at ${writeDir}${batchName}_Syrah_results_summary.pdf"
     if [ "$doNonSyrah" = true ]; then
-      echo "and for the non-Syrah version at ${writeDir}{batchName}_nonSyrah_results_summary.pdf"
+      echo "and for the non-Syrah version at ${writeDir}${batchName}_nonSyrah_results_summary.pdf"
     fi
   else
     echo ""
