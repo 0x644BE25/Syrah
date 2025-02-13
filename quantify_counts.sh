b1687d6c9b84245eed018573d1cbd81ab955b770
Carolyn Brewster <carolynbrewster@gmail.com>
2025-02-12 19:13:41 -0600

    Update quantify_counts.sh

--- quantify_counts.sh
+++ quantify_counts.sh
@@ -14,22 +14,22 @@ source "$manifestFile"
 
 # this is the step that requires subread
 featureCounts -a "${gtf}" \
-              -o "${batchName}_gene_assigned" \
-              -R BAM "${batchName}_Aligned.sortedByCoord.out.bam" \
+              -o "${writeDir}intermediate_files/${batchName}_gene_assigned" \
+              -R BAM "${writeDir}intermediate_files/${batchName}_Aligned.sortedByCoord.out.bam" \
               -T "${nCores}";
               
-samtools sort "${batchName}_Aligned.sortedByCoord.out.bam.featureCounts.bam" -o "${batchName}_assigned_sorted.bam"
-samtools index "${batchName}_assigned_sorted.bam"
-umi_tools count --wide-format-cell-counts --per-gene --gene-tag=XT --assigned-status-tag=XS --per-cell -I "${batchName}_assigned_sorted.bam" -S "${writeDir}${batchName}_counts.tsv.gz"
+samtools sort "${writeDir}intermediate_files/${batchName}_Aligned.sortedByCoord.out.bam.featureCounts.bam" -o "${writeDir}intermediate_files/${batchName}_assigned_sorted.bam"
+samtools index "${writeDir}intermediate_files/${batchName}_assigned_sorted.bam"
+umi_tools count --wide-format-cell-counts --per-gene --gene-tag=XT --assigned-status-tag=XS --per-cell -I "${writeDir}intermediate_files/${batchName}_assigned_sorted.bam" -S "${writeDir}${batchName}_counts.tsv.gz"
 
               
 if [ $doNonSyrah = true ]; then
   featureCounts -a "${gtf}" \
-                -o "${batchName}_nonSyrah_gene_assigned" \
-                -R BAM "${batchName}_nonSyrah_Aligned.sortedByCoord.out.bam" \
+                -o "${writeDir}intermediate_files/${batchName}_nonSyrah_gene_assigned" \
+                -R BAM "${writeDir}intermediate_files/${batchName}_nonSyrah_Aligned.sortedByCoord.out.bam" \
                 -T "${nCores}";
                 
-  samtools sort "${batchName}_nonSyrah_Aligned.sortedByCoord.out.bam.featureCounts.bam" -o "${batchName}_nonSyrah_assigned_sorted.bam"
-  samtools index "${batchName}_nonSyrah_assigned_sorted.bam"
-  umi_tools count --wide-format-cell-counts --per-gene --gene-tag=XT --assigned-status-tag=XS --per-cell -I "${batchName}_nonSyrah_assigned_sorted.bam" -S "${writeDir}${batchName}_nonSyrah_counts.tsv.gz"
+  samtools sort "${writeDir}intermediate_files/${batchName}_nonSyrah_Aligned.sortedByCoord.out.bam.featureCounts.bam" -o "${writeDir}intermediate_files/${batchName}_nonSyrah_assigned_sorted.bam"
+  samtools index "${writeDir}intermediate_files/${batchName}_nonSyrah_assigned_sorted.bam"
+  umi_tools count --wide-format-cell-counts --per-gene --gene-tag=XT --assigned-status-tag=XS --per-cell -I "${writeDir}intermediate_files/${batchName}_nonSyrah_assigned_sorted.bam" -S "${writeDir}${batchName}_nonSyrah_counts.tsv.gz"
 fi
