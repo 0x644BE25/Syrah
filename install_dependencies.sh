668deaca572b1d06160b17aef5b1f8ba122c43de
Carolyn Brewster <carolynbrewster@gmail.com>
2025-03-14 08:19:58 -0500

    add dbscan

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -13,6 +13,10 @@ else
   exit 1
 fi
 
+# ================= DBSCAN ===========================
+
+echo "install.packages('dbscan',repos='http://cran.us.r-project.org')" | R --vanilla
+
 # ================= samtools =========================
 
 curl -O -L "https://github.com/samtools/samtools/releases/download/1.20/samtools-1.20.tar.bz2"
